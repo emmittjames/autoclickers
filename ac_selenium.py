b77c7f016732a09203ceec4b6952b442d4313f94
emmittjames <emmittjames1@gmail.com>
2024-09-10 00:59:06 -0400

    switched to refreshing the page and clearing local storage, much faster now

--- ac_selenium.py
+++ ac_selenium.py
@@ -15,39 +15,42 @@ def switch_to_deepest_iframe(driver):
             print(f"Error: {e}")
             break
 
-def main():
-    options = webdriver.FirefoxOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Firefox(options=options)
-    driver.get("https://allstatesugarbowl.org/news/2024/9/9/manning-stars.aspx")
-    time.sleep(0.5)
+def click_on_ac(driver):
+    driver.execute_script("window.localStorage.clear();")
+    driver.refresh()
+    time.sleep(1)
     switch_to_deepest_iframe(driver)
     try:
         colandrea = driver.find_element(By.ID, "1ab96bb1-6eb5-11ef-9287-49c1ce55de26")
         colandrea.click()
         print("clicked")
-        time.sleep(0.1)
+        time.sleep(0.5)
+        return True
     except Exception as e:
         print(f"Error: {e}")
         return False
+
+def main():
+    options = webdriver.FirefoxOptions()
+    driver = webdriver.Firefox(options=options)
+
+    successes = 0
+    failures = 0
+    start_time = time.time()
+
+    driver.get("https://allstatesugarbowl.org/news/2024/9/9/manning-stars.aspx")
+    time.sleep(1)
+    for _ in range(0, 1000):
+        if click_on_ac(driver):
+            successes += 1
+        else:
+            failures += 1
+        print(f"Successes: {successes}, Failures: {failures}")
+        print(f"Successes per minute: {successes / ((time.time() - start_time) / 60)}")
+        print(f"Elapsed time: {(time.time() - start_time) / 60} minutes")
+
     driver.quit()
     return True
 
 if __name__ == '__main__':
-    successes = 0
-    failures = 0
-    start_time = time.time()
-    for _ in range(0, 10000):
-        try:
-            if main():
-                successes += 1
-            else:
-                failures += 1
-        except Exception as e:
-            print(f"Error: {e}")
-            break
-        elapsed_time = time.time() - start_time
-        print(f"Successes: {successes}")
-        print(f"Failures: {failures}")
-        print(f"Successes per minute: {successes / elapsed_time * 60}")
-        print(f"Total Time: {elapsed_time/60:.2f} minutes")
+    main()
