6c5d2416420dfe1216d405c0bba2d4adc14479b5
emmittjames <emmittjames1@gmail.com>
2024-09-09 22:47:33 -0400

    added failure tracking

--- ac_selenium.py
+++ ac_selenium.py
@@ -35,11 +35,15 @@ def main():
 
 if __name__ == '__main__':
     successes = 0
+    failures = 0
     for _ in range(0, 10000):
         try:
             if main():
                 successes += 1
+            else:
+                failures += 1
         except Exception as e:
             print(f"Error: {e}")
             break
         print(f"Successes: {successes}")
+        print(f"Failures: {failures}")
